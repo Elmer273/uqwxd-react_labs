adf0feebcbbce1cc5a5a8f7ec797a3fa3233af81
Elmer273 <elmermartinez666@yahoo.com>
2023-09-18 15:03:52 -0400

    Implement Redux

--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
+import {legacy_createStore as createStore} from 'redux';
 
-import {createStore} from 'redux';
 import {Provider} from 'react-redux'
 import myReducers from './reducers'
 
